25dec48766b5f6e77feae29f6da897a46ed4a27a
Justin Hurt <hurtsec@gmail.com>
2023-04-21 03:08:35 +0000

    test copied install file

--- install.sh
+++ install.sh
@@ -1,22 +1,26 @@
 #!/bin/bash
 
+
 create_symlinks() {
-  # Get the directory in which this script lives.
-  script_dir=$(dirname "$(readlink -f "$0")")
+    # Get the directory in which this script lives.
+    script_dir=$(dirname "$(readlink -f "$0")")
 
-  # Get a list of all files in this direcotry that start with a dot.
-  files=$(find -maxdepth 1 -type f -name ".*")
+    # Get a list of all files in this directory that start with a dot.
+    files=$(find -maxdepth 1 -type f -name ".*")
 
-  # Create a symbolic link to each file in the home directory.
-  for file in $files; do
-    name=$(basename $file)
-    echo "Creating symlink to $name in home directory."
-    rm -rf ~/$name
-    ln -s $script_dir/$name ~/$name
-  done
+    # Create a symbolic link to each file in the home directory.
+    for file in $files; do
+        name=$(basename $file)
+        echo "Creating symlink to $name in home directory."
+        rm -rf ~/$name
+        ln -s $script_dir/$name ~/$name
+    done
 }
 
 create_symlinks
+
+echo "Initializing conda for zsh."
+conda init zsh
 
 echo "Installing fonts."
 FONT_DIR="$HOME/.fonts"
